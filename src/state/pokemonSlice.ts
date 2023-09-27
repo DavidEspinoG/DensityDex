c5437a3d3047ad9bd1c217cb99a36cb878186370
David Espino <davidespinog@gmail.com>
2023-09-27 17:09:55 -0600

    Add previous page and next page to state

--- src/state/pokemonSlice.ts
+++ src/state/pokemonSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { apiUrls } from "../constants";
+
 const fetchPokemons = createAsyncThunk(
   'pokemons/fetchPokemons', async() => {
     const res = await axios.get(apiUrls.getPokemons);
@@ -14,13 +15,17 @@ interface PokemonsState {
   loading: boolean,
   imageUrl: string | null, 
   detailedPokemon: object,
+  prevPage: string | null,
+  nextPage: string | null, 
 }
 
 const initialState: PokemonsState = {
   currentPokemons: [],
   detailedPokemon: {},
   loading: false, 
-  imageUrl: null
+  imageUrl: null, 
+  prevPage: '', 
+  nextPage: '',
 };
 
 const pokemonsSlice = createSlice({
@@ -32,9 +37,15 @@ const pokemonsSlice = createSlice({
     .addCase(fetchPokemons.pending, (state) => {
       state.loading = true;
     })
-    .addCase(fetchPokemons.fulfilled, (state, action : PayloadAction) => {
+    .addCase(fetchPokemons.fulfilled, (state, action) => {
       state.loading = false; 
-      state.currentPokemons = action.payload;
+      state.currentPokemons = action.payload.results;
+      if(action.payload.results[0].name !== 'example'){
+        state.nextPage = action.payload.next;
+      } else {
+        state.nextPage = null;
+      }
+      state.prevPage = action.payload.previous;
     })
   }
 });
